e2e2373dac031d95cb9a9357fc9c47e26f32313e
Carlos Ribeiro <carlos.ribeiro@criticaltechworks.com>
2019-11-03 03:36:43 +0000

    first commit

---
+++ fcounter/js/main.js
@@ -0,0 +1,30 @@
+var counter = 0;
+
+var badges = {
+    "1": "badge_1",
+    "5": "badge_2",
+    "10": "badge_3",
+    "15": "badge_4",
+    "20": "badge_5",
+    "25": "badge_6",
+}
+
+function incrementCounter(){
+    var counterHolder = document.getElementById("counter");
+    counter = counterHolder.innerText;
+    counter++;
+    counterHolder.innerText = counter;
+
+    checkBadges();
+}
+
+
+function checkBadges(){
+
+    if(counter.toString() in badges){
+        var badgeName = badges[counter.toString()];
+        var badge = document.getElementById(badgeName);
+        badge.classList.remove("invisible");
+        badge.classList.add('animated', 'zoomInDown');
+    }
+}
